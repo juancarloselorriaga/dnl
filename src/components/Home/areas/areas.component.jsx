09f83fa46f8af82db00468132daa00e56d419af0
juancarloselorriaga <juancarloselorriaga@gmail.com>
2019-09-25 23:27:21 -0500

    imagenes de áreas conectadas a gatsby image con graphql

--- src/components/Home/areas/areas.component.jsx
+++ src/components/Home/areas/areas.component.jsx
@@ -1,16 +1,44 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Title from "../../title/title.component"
 import SectionDescription from "../../section-description/section-description.component"
 import Seccion from "../../seccion/seccion.component"
 import Center from "../../center/center.component"
-import { FaLongArrowAltRight } from "react-icons/fa"
-
-import areas from "../../../constants/services/areas"
+import Servicio from "../../servicio/servicio.component"
 
 import styles from "./areas.module.css"
 
+const getImages = graphql`
+  query Image {
+    legal: file(relativePath: { eq: "legal.png" }) {
+      childImageSharp {
+        fixed(width: 100, height: 100) {
+          ...GatsbyImageSharpFixed_withWebp_tracedSVG
+        }
+      }
+    }
+    web: file(relativePath: { eq: "webIcon.png" }) {
+      childImageSharp {
+        fixed(width: 100, height: 100) {
+          ...GatsbyImageSharpFixed_withWebp_tracedSVG
+        }
+      }
+    }
+    synergy: file(relativePath: { eq: "synergyIcon.png" }) {
+      childImageSharp {
+        fixed(width: 100, height: 100) {
+          ...GatsbyImageSharpFixed_withWebp_tracedSVG
+        }
+      }
+    }
+  }
+`
+
 const Areas = () => {
+  const { legal, web, synergy } = useStaticQuery(getImages)
+
   return (
     <Seccion offWhite>
       <Title title="Una solución completa a tus necesidades" />
@@ -20,18 +48,24 @@ const Areas = () => {
       />
       <Center background>
         <div className={styles.center}>
-          {areas.map((item, index) => {
-            return (
-              <article key={index} className={styles.area}>
-                <div className={styles.iconWrapper}>{item.icon}</div>
-                <p>{item.text}</p>
-                <a href="/">
-                  <span>{item.title}</span>
-                  <FaLongArrowAltRight />
-                </a>
-              </article>
-            )
-          })}
+          <Servicio
+            title="DNL Legal"
+            text="Dreamcatcher intelligentsia roof party woke shaman, green juice la croix kogi selvage."
+          >
+            <Img fixed={legal.childImageSharp.fixed} />
+          </Servicio>
+          <Servicio
+            title="DNL Web"
+            text="ianbing cloud bread cardigan taxidermy poke, eytar copper mug truffaut live-edge"
+          >
+            <Img fixed={web.childImageSharp.fixed} />
+          </Servicio>
+          <Servicio
+            title="DNL Synergy"
+            text="Synth tbh copper mug gentrify. Tattooed occupy meggings yuccie snackwave meh pinterest seitan. "
+          >
+            <Img fixed={synergy.childImageSharp.fixed} />
+          </Servicio>
         </div>
       </Center>
     </Seccion>
---
+++ src/components/servicio/servicio.component.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import styled from "styled-components"
+
+import { FaLongArrowAltRight } from "react-icons/fa"
+
+const Servicio = ({ children, text, title, className }) => {
+  return (
+    <article className={className}>
+      <div className="icon-wrapper">{children}</div>
+      <p>{text}</p>
+      <a href="/">
+        <span>{title}</span>
+        <FaLongArrowAltRight />
+      </a>
+    </article>
+  )
+}
+
+export default styled(Servicio)`
+  margin: 2rem 0 1rem 0;
+  padding-bottom: 1rem;
+  text-align: center;
+  border-bottom: 0.5px rgb(0, 0, 0, 0.3) solid;
+  span {
+    color: var(--primaryColor);
+    padding: 0.5rem;
+    display: inline-block;
+  }
+
+  a {
+    width: 100%;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 0.8rem;
+    margin: 1rem;
+    color: var(--primaryColor);
+  }
+
+  .icon-wrapper {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 2rem;
+  }
+`
