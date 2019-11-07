3b86dc848bd6ddd1e5558fd9396acb09aee96196
juancarloselorriaga <juancarloselorriaga@gmail.com>
2019-11-06 19:11:19 -0600

    add fb link

--- src/constants/social-icons/social-icons.constant.js
+++ src/constants/social-icons/social-icons.constant.js
@@ -3,7 +3,7 @@ import React from "react"
 import { FaFacebook, FaTwitterSquare, FaInstagram, FaEnvelope } from "react-icons/fa"
 
 export default [
-  { icon: <FaFacebook />, url: "https//facebook.com", title: 'facebook' },
+  { icon: <FaFacebook />, url: "https://www.facebook.com/contactodnlomas/", title: 'facebook' },
   { icon: <FaTwitterSquare />, url: "https//twitter.com", title: 'twitter' },
   { icon: <FaInstagram />, url: "https//instagram.com", title: 'instagram' },
   { icon: <FaEnvelope />, url: "mailto:contacto@dnlomas.mx", title: 'mail' },
